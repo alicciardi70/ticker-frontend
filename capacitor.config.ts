9f5dbfda480f64dd4a5f79816d61939ae0adc1a6
alicciardi70 <44737600+alicciardi70@users.noreply.github.com>
2025-12-20 10:36:11 -0500

    changes for phone, emulator

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tickerink.app',
   appName: 'Ticker Ink',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: "http://192.168.68.61:5173",
+    cleartext: true
+  }
 };
 
 export default config;
--- startfrontend.bat
+++ startfrontend.bat
@@ -1,2 +1,2 @@
 cd C:\hedgefund\tickerview\website\frontend
-npm run dev
+npm run dev -- --host 0.0.0.0 --port 5173
