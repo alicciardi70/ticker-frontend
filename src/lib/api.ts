c6b2cf49116fd8e6db3257283b9d12fc44484a42
alicciardi70 <44737600+alicciardi70@users.noreply.github.com>
2025-12-19 23:32:36 -0500

    clean api.ts to use env variable)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,1 @@
-// src/lib/api.ts
-//export const API_BASE =  import.meta.env.VITE_API_BASE ?? "http://127.0.0.1:8000";
-
-
-export const API_BASE =
-  import.meta.env.VITE_API_BASE ?? "http://192.168.68.61:8000";
+export const API_BASE = import.meta.env.VITE_API_BASE;
